f6c90975eb8ae8e52abb953b8fdfef57a9fbcaa9
Test <raphealsiew@gmail.com>
2025-08-11 21:27:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,57 @@
-const images = [
-    "https://i.postimg.cc/2ypJ1WSR/Yogi-Blueberry-Cream-Stick-32g-4.png",
-    "https://i.postimg.cc/fWXp688w/Yogi-Cookies-Cream-Stick-36g.png",
-    "https://i.postimg.cc/MpRnR2DC/Amore-Oat-Cookies-Blackcurrant-Chocolate-Chips-162g-1.png",
-    "https://i.postimg.cc/dtg7Lt8R/Amore-Oat-Cookies-Blackcurrant-162g-1.png",
-    "https://i.postimg.cc/KY2RDqpR/Amore-Oat-Cookies-Chocolate-Chips-162g-1.png",
-    "https://i.postimg.cc/W3c6rwgn/NOI-Cassava-Chips-Salted-85g.png",
-    "https://i.postimg.cc/bvt9sBDr/NOI-Cassava-Chips-Seaweed-Wasabi-85g.png"
-];
+ const imageLinks = [
+        "https://i.postimg.cc/2ypJ1WSR/Yogi-Blueberry-Cream-Stick-32g-4.png",
+        "https://i.postimg.cc/fWXp688w/Yogi-Cookies-Cream-Stick-36g.png",
+        "https://i.postimg.cc/MpRnR2DC/Amore-Oat-Cookies-Black.png",
+        "https://i.postimg.cc/N0f4S9zN/Yogi-Strawberry-Cream-Stick-32g.png",
+        "https://i.postimg.cc/TY8FmgGW/Amore-Oat-Cookies-White.png",
+        "https://i.postimg.cc/ZnVY5Q2d/Yogi-Chocolate-Cream-Stick-36g.png",
+        "https://i.postimg.cc/4yC2rYjS/Amore-Oat-Cookies-Red.png"
+    ];
 
-// Duplicate array to make pairs
-let gameImages = [...images, ...images];
+    // Select 6 random images from list
+    let selectedImages = imageLinks
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 6);
 
-// Shuffle function
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+    let gameImages = [...selectedImages, ...selectedImages].sort(() => 0.5 - Math.random());
+
+    const gameContainer = document.getElementById("game");
+
+    let firstCard = null;
+    let lockBoard = false;
+
+    function createCard(image) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <img class="front" src="https://via.placeholder.com/100/cccccc/000000?text=?" alt="Front">
+            <img class="back" src="${image}" alt="Back">
+        `;
+        card.addEventListener("click", () => flipCard(card, image));
+        gameContainer.appendChild(card);
     }
-}
-shuffle(gameImages);
 
-const gameBoard = document.getElementById('gameBoard');
-let firstCard = null;
-let lockBoard = false;
+    function flipCard(card, image) {
+        if (lockBoard || card.classList.contains("flip")) return;
 
-// Create cards
-gameImages.forEach(src => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-        <div class="card-inner">
-            <div class="card-front">
-                <img src="${src}" alt="card image">
-            </div>
-            <div class="card-back"></div>
-        </div>
-    `;
-    card.addEventListener('click', () => flipCard(card, src));
-    gameBoard.appendChild(card);
-});
+        card.classList.add("flip");
 
-function flipCard(card, src) {
-    if (lockBoard || card.classList.contains('flipped')) return;
+        if (!firstCard) {
+            firstCard = { card, image };
+            return;
+        }
 
-    card.classList.add('flipped');
-
-    if (!firstCard) {
-        firstCard = { card, src };
-    } else {
-        if (firstCard.src === src) {
-            // Match found
+        if (firstCard.image === image) {
             firstCard = null;
         } else {
-            // No match - flip back
             lockBoard = true;
             setTimeout(() => {
-                card.classList.remove('flipped');
-                firstCard.card.classList.remove('flipped');
+                card.classList.remove("flip");
+                firstCard.card.classList.remove("flip");
                 firstCard = null;
                 lockBoard = false;
             }, 1000);
         }
     }
-}
+
+    gameImages.forEach(img => createCard(img));
